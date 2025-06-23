289e7bddfbdce747c930b019b2736771e97d80e5
Alexander Hübert <contact@huebi.dev>
2025-06-23 14:08:05 +0200

    log: read batch first, parse after

--- leveldb-parser-lib/src/log_parser.rs
+++ leveldb-parser-lib/src/log_parser.rs
@@ -11,9 +11,43 @@ pub fn parse_file(file_path: &str) -> io::Result<()> {
     let mut reader = BufReader::new(file);
 
     let mut block_counter = 1;
+    let mut partial_block_data = Vec::new();
+
+    let mut first_block_offset = 0;
+
     while reader.stream_position()? < file_size {
         let block = read_block(&mut reader)?;
-        print_block(&block, block_counter)?;
+
+        print_block_header(&block, block_counter)?;
+
+        // TODO: calc of key offset will be off when entry spans multiple blocks...
+        match block.block_type {
+            1 => {
+                // Full Block
+                partial_block_data.clear();
+                process_batch(&block.data, block.offset)?;
+            }
+            2 => {
+                // First Block
+                first_block_offset = block.offset;
+                partial_block_data.clear();
+                partial_block_data.extend_from_slice(&block.data);
+            }
+            3 => {
+                // Middle Block
+                partial_block_data.extend_from_slice(&block.data);
+            }
+            4 => {
+                // Last Block
+                partial_block_data.extend_from_slice(&block.data);
+                process_batch(&partial_block_data, first_block_offset)?;
+                partial_block_data.clear();
+            }
+            _ => {
+                // Zero Block or Unknown Type
+                println!("Block Type {} is not processed.", block.block_type);
+            }
+        }
 
         block_counter += 1;
     }
@@ -49,27 +83,6 @@ pub fn read_block(reader: &mut (impl Read + Seek)) -> io::Result<Block> {
     })
 }
 
-struct BatchHeader {
-    seq_no: u64,
-    num_records: u32,
-}
-
-fn read_batch_header(reader: &mut (impl Read + Seek)) -> io::Result<BatchHeader> {
-    let seq_no = reader.read_u64::<LittleEndian>()?;
-    let num_records = reader.read_u32::<LittleEndian>()?;
-
-    Ok(BatchHeader {
-        seq_no,
-        num_records,
-    })
-}
-
-fn print_block(block: &Block, block_counter: u64) -> io::Result<()> {
-    print_block_header(block, block_counter)?;
-    print_block_data(block)?;
-    Ok(())
-}
-
 pub fn print_block_header(block: &Block, block_counter: u64) -> io::Result<()> {
     println!(
         "\n########## [ Block {} (Offset: {})] ############",
@@ -97,45 +110,73 @@ pub fn print_block_header(block: &Block, block_counter: u64) -> io::Result<()> {
     Ok(())
 }
 
-fn print_block_data(block: &Block) -> io::Result<()> {
-    let mut cursor = Cursor::new(&block.data);
-    if block.block_type == 1 || block.block_type == 2 {
-        println!("---------------- Batch Header ----------------");
+fn process_batch(data: &[u8], offset: u64) -> io::Result<()> {
+    println!("---------------- Batch Header ----------------");
 
-        let batch_header = read_batch_header(&mut cursor)?;
-        println!("Sequence: {}", batch_header.seq_no);
-        println!("Records: {}", batch_header.num_records);
+    let mut cursor = Cursor::new(data);
+    let batch_header = read_batch_header(&mut cursor)?;
+    println!("Seq: {}", batch_header.seq_no);
+    println!("Records: {}", batch_header.num_records);
 
-        for i in 0..batch_header.num_records {
-            let record_state = cursor.read_u8()?;
-
-            match record_state {
-                0 => println!("\n******* Record {} (State: 0 [Deleted]) ********", i + 1),
-                1 => println!("\n********* Record {} (State: 1 [Live]) *********", i + 1),
-                _ => println!("\n*********** Unknown Record State *************"),
-            }
-
-            let key_len = utils::read_varint(&mut cursor)?;
-            // calc key offset from block start + header + cursor position
-            let key_offset = block.offset + 7 + cursor.position();
-            let key = utils::read_slice(&mut cursor, key_len as usize)?;
-
-            println!(
-                "--------- Key (Offset: {}, Size: {}) ---------",
-                key_offset,
-                key.len()
-            );
-            println!("{}", utils::bytes_to_ascii_with_hex(&key));
-
-            if record_state != 0 {
-                let value = utils::read_varint_slice(&mut cursor)?;
-                println!("------------- Value (Size: {}) --------------", value.len());
-                println!("{}", utils::bytes_to_ascii_with_hex(&value));
-            }
+    for i in 0..batch_header.num_records {
+        if cursor.position() >= data.len() as u64 {
+            println!("Unexpected end of data at record: {}", i + 1);
+            break;
         }
-    } else if block.block_type == 3 || block.block_type == 4 {
-        println!("------------------- Value --------------------");
-        println!("{}", utils::bytes_to_ascii_with_hex(&block.data));
+        process_single_record(&mut cursor, offset, i)?;
+    }
+
+    Ok(())
+}
+
+struct BatchHeader {
+    seq_no: u64,
+    num_records: u32,
+}
+
+fn read_batch_header(reader: &mut (impl Read + Seek)) -> io::Result<BatchHeader> {
+    let seq_no = reader.read_u64::<LittleEndian>()?;
+    let num_records = reader.read_u32::<LittleEndian>()?;
+
+    Ok(BatchHeader {
+        seq_no,
+        num_records,
+    })
+}
+
+fn process_single_record(cursor: &mut Cursor<&[u8]>, block_offset: u64, i: u32) -> io::Result<()> {
+    println!("\n****************** Record {} ******************", i + 1);
+    let record_state = cursor.read_u8()?;
+
+    let key_len = utils::read_varint(cursor)?;
+
+    // calc key offset from block start + header + header in between + cursor position
+    let key_offset = block_offset + 7 + cursor.position();
+    let key = utils::read_slice(cursor, key_len as usize)?;
+
+    println!(
+        "State: {}",
+        match record_state {
+            0 => "0 (Deleted)",
+            1 => "1 (Live)",
+            _ => "Unknown",
+        },
+    );
+
+    println!(
+        "Key (Offset: {}, Size: {}): '{}'",
+        key_offset,
+        key.len(),
+        utils::bytes_to_ascii_with_hex(&key)
+    );
+
+    if record_state != 0 {
+        let value = utils::read_varint_slice(cursor)?;
+        println!(
+            "Val (Size: {}): '{}'",
+            value.len(),
+            utils::bytes_to_ascii_with_hex(&value)
+        );
     }
 
     Ok(())
